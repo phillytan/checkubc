f2d79f8753c7c5a65ed01519a0c2254b3e2cc10b
Phillbert Tan <mrsilver512@users.noreply.github.com>
2019-06-19 08:30:54 +0800

    Bug Fixes

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ let courses = [
     {
         dept: "ECON",
         class: "101",
-        section: "001",
+        section: "003",
         restricted: false,
     },
     {
@@ -72,30 +72,35 @@ courses.forEach(response => {
                 general: ((browser.text('div.content > table:nth-of-type(4) > tbody > tr:nth-of-type(3) > td:nth-of-type(2) > strong') === "") ? browser.text('div.content > table:nth-of-type(3) > tbody > tr:nth-of-type(3) > td:nth-of-type(2) > strong') : browser.text('div.content > table:nth-of-type(4) > tbody > tr:nth-of-type(3) > td:nth-of-type(2) > strong')),
             }
         };
+        let num = (response.restricted ? classData.course_seats.total : classData.course_seats.general);
+        let current = num;
+        if (num > 20) current = chalk.bold(chalk.green(num));
+        else if (num > 10) current = chalk.bold(chalk.yellow(num));
+        else current = chalk.bold(chalk.red(num));
+        let classInfo = chalk.cyan(response.dept + " " + response.class + " " + response.section);
         let filename = __dirname+"/db/"+response.dept + " " + response.class + " " + response.section + " " + response.restricted;
         if ((response.restricted ? classData.course_seats.total : classData.course_seats.general) == "") {
-            if (browser.text('div.content > strong') === "Note: The remaining seats in this section are only available through a Standard Timetable (STT)") console.log(`${chalk.cyan(response.dept + " " + response.class + " " + response.section)} is only available through a ${chalk.yellow("Standard Timetable (STT)")}.`);
-            else if (!fs.existsSync(filename)) console.log(`Sorry but I couldn't check the number of seats available for ${chalk.cyan(response.dept + " " + response.class + " " + response.section)}.`);
+            if (browser.text('div.content > strong') === "Note: The remaining seats in this section are only available through a Standard Timetable (STT)") console.log(`${classInfo} is only available through a ${chalk.yellow("Standard Timetable (STT)")}.`);
+            else if (!fs.existsSync(filename)) console.log(`Sorry but I couldn't check the number of seats available for ${classInfo}.`);
             else {
                 fs.readFile(filename, "utf-8", (err, data) => {
                     let lastSaved = Number(data.split(" ")[data.split(" ").length-1]);
-                    console.log(`Sorry but I couldn't check the number of seats available for ${chalk.cyan(response.dept + " " + response.class + " " + response.section)}. There were ${chalk.yellow(lastSaved)} seats the last time I checked.`);
+                    console.log(`Sorry but I couldn't check the number of seats available for ${classInfo}. There were ${chalk.yellow(lastSaved)} seats the last time I checked.`);
                 });
             }
         } else {
             if (!fs.existsSync(filename)) {
                 fs.writeFile(filename, (response.restricted ? classData.course_seats.total : classData.course_seats.general), () => {
-                    console.log(`As of the moment, ${chalk.cyan(response.dept + " " + response.class + " " + response.section)} has ${chalk.yellow(response.restricted ? classData.course_seats.total : classData.course_seats.general)} seats remaining.`);
+                    console.log(`As of the moment, ${classInfo} has ${current} seats remaining.`);
                 });
             } else {
                 fs.readFile(filename, "utf-8", (err, data) => {
                     let lastSaved = Number(data.split(" ")[data.split(" ").length-1]); 
                     if (lastSaved !== Number(response.restricted ? classData.course_seats.total : classData.course_seats.general)) {
-                        data = data + ` ${(response.restricted ? classData.course_seats.total : classData.course_seats.general)}`;
-                        fs.writeFile(filename, data, (err) => {if (err) console.error(err)});
-                        console.log(`As of the moment, ${chalk.cyan(response.dept + " " + response.class + " " + response.section)} has ${chalk.green(response.restricted ? classData.course_seats.total : classData.course_seats.general)} seats remaining. There were ${chalk.yellow(lastSaved)} seats the last time I checked.`);
+                        fs.writeFile(filename, (data + ` ${num}`), (err) => {if (err) console.error(err)});
+                        console.log(`As of the moment, ${classInfo} has ${current} seats remaining. There were ${chalk.yellow(lastSaved)} seats the last time I checked.`);
                     } else {
-                        console.log(`As of the moment, ${chalk.cyan(response.dept + " " + response.class + " " + response.section)} has ${chalk.green(response.restricted ? classData.course_seats.total : classData.course_seats.general)} seats remaining. ${chalk.red("No changes")} since the last time I checked.`);
+                        console.log(`As of the moment, ${classInfo} has ${current} seats remaining. ${chalk.red("No changes")} since the last time I checked.`);
                     }
                 });
             }
